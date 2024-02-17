32315e0887e156a72fa16a362df8b3885120ac27
srikar <msrikar0403@gmail.com>
2024-02-17 19:07:12 +0530

    trying in render

--- server/index.js
+++ server/index.js
@@ -13,10 +13,12 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
-app.use(cors({
-  origin: 'http://localhost:3000', // Replace with your frontend's actual origin
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'http://localhost:3000', // Replace with your frontend's actual origin
+//   credentials: true,
+// }));
+
+app.use(cors());
 
 app.use('/posts',postRoutes);
 
